0bcc0e0406f8a4657d9b4cc9d1e89ca55ab55a79
Ironhack <ironhack@Ironhacks-MacBook-Pro.local>
2024-08-13 10:25:55 +0200

    day 1 solved

--- server/app.js
+++ server/app.js
@@ -1,36 +1,55 @@
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const PORT = 5005;
+const express = require("express")
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+const cors = require('cors')
+const PORT = 5005
 
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
-
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
-const app = express();
+const app = express()
 
 
-// MIDDLEWARE
-// Research Team - Set up CORS middleware here:
-// ...
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: ["http://localhost:5173"]
+  })
+)
 
 
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
-app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
-});
 
 
-// START SERVER
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+app.get('/api/cohorts', (req, res) => {
+  const cohorts = require('./cohorts.json')
+  res.json(cohorts)
+})
+
+
+
+
+app.get('/api/students', (req, res) => {              // http://localhost:5005/api/students
+  const students = require('./students.json')
+  res.json(students)
+})
+
+app.get('/api/students/search', (req, res) => {       // http://localhost:5005/api/students/search?language=javascript
+  const { language } = req.query
+  res.send(`Debo buscar en la BBDD los alumnos/as que controlen de ${language}`)
+})
+
+app.get('/api/students/:studentId', (req, res) => {   // http://localhost:5005/api/students/674836583
+  const { studentId } = req.params
+  res.send(`Debo buscar en la BBDD el Id de alumno ${studentId}`)
+})
+
+
+
+
+
+app.get('*', (req, res) => {
+  res.sendStatus(404)
+})
+
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
